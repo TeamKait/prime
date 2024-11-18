e9e6f3dc6ec79828b979f812035658db173e0c0f
TeamKait <teamkait1@gmail.com>
2024-11-18 13:22:32 +0300

    Add files via upload
    init

---
+++ Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace _38
+{
+    class Temperature
+    {
+        public int day;
+        public int temp;
+
+        public Temperature(int day, int temp)
+        {
+            this.day = day;
+            this.temp = temp;
+        }
+    }
+    internal class Program
+    {
+        static String _DIR = @"S:\ИСП211\Дерин Лющенко\Практика Учебная\Дерин\38";
+        static string filename(string name, string separator = @"\")
+        {
+            return _DIR + separator + name;
+        }
+        static void openSheetAndProcess(Excel.Workbook xlWorkbook, ref Collection<Temperature> temperatures, int sheet)
+        {
+            Console.WriteLine("Открываю лист номер " + sheet);
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[sheet];
+
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+            int rowCount = xlRange.Rows.Count;
+            int colCount = xlRange.Columns.Count;
+
+            int j = 3;
+            Temperature currentTemp = new Temperature(0,0);
+            Console.WriteLine("Начинаю парсинг");
+            for (int i = 1; i <= rowCount; i++)
+            {
+                
+                if (xlRange.Cells[i, 1] != null && xlRange.Cells[i, 1].Value2 != null) 
+                {
+                    try
+                    {
+                        string temp = xlRange.Cells[i, 1].Value2.ToString();
+                        if (!double.TryParse(temp, out double _))
+                        {
+                            int date = Int32.Parse(temp.Split('.')[0]);
+                            currentTemp.day = date;
+
+                            temperatures.Add(currentTemp);
+                            currentTemp = new Temperature(0, 0);
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Т");
+                    }
+                   
+                }
+                    
+                if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                {
+                    try
+                    {
+                        int temp = (int)xlRange.Cells[i, j].Value2;
+
+                        currentTemp.temp = temp;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Т");
+                    }
+
+                }
+                Console.WriteLine("Парсинг строки " + i + " завершен");
+            }
+
+            Console.WriteLine("Завершил парсинг листа.");
+        }
+        static void ExcelExport(string file, dynamic[,] data)
+        {
+            object oMissing = System.Reflection.Missing.Value;
+
+            Excel.Application excelApp = null;
+            Excel.Range range = null;
+            Excel.Workbook workbook = null;
+            Excel.Worksheet worksheet = null;
+
+            int worksheetCount = 0;
+
+            try
+            {
+
+                excelApp = new Excel.Application();
+
+                excelApp.DisplayAlerts = false;
+
+                excelApp.Visible = false;
+
+                workbook = excelApp.Workbooks.Add();
+
+                worksheetCount = workbook.Sheets.Count;
+
+                worksheet = workbook.Sheets.Add();
+
+                if (data != null)
+                {
+                    for (int i = 0; i < data.GetLength(0); i++)
+                    {
+                        int rowNum = i + 1;
+
+                        for (int j = 0; j < data.GetLength(1); j++)
+                        {
+                            int colNum = j + 1;
+
+                            //set cell location that data needs to be written to
+                            //range = worksheet.Cells[rowNum, colNum];
+
+                            //set value of cell
+                            //range.Value = data[i,j];
+
+                            //set value of cell
+                            worksheet.Cells[rowNum, colNum] = data[i, j];
+                        }
+                    }
+                }
+
+                workbook.SaveAs(filename(file), System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value, Excel.XlSaveAsAccessMode.xlNoChange, System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value);
+
+                Console.WriteLine("Status: Complete. " + DateTime.Now.ToString("HH:mm:ss"));
+
+            }
+            catch (Exception ex)
+            {
+                string errMsg = "Error (WriteToExcel) - " + ex.Message;
+                System.Diagnostics.Debug.WriteLine(errMsg);
+            }
+            finally
+            {
+                if (workbook != null)
+                {
+                    workbook.Close();
+
+                    System.Runtime.InteropServices.Marshal.FinalReleaseComObject(workbook);
+                }
+
+                if (excelApp != null)
+                {
+                    excelApp.Quit();
+
+                    System.Runtime.InteropServices.Marshal.FinalReleaseComObject(excelApp);
+                }
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            Utils.info = new Utils.ProgramInfo(
+              author: "Дерин Владислав",
+              name: "38. Статистический анализ",
+              description: "Работа с Excel",
+              instruction: "");
+            //Utils.PrintAuthors();
+            Console.WriteLine("Открываю файл...");
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(filename("2019.xlsx"));
+            Console.WriteLine("Файл открыт");
+            Collection<Temperature> temperaturesFirst = new Collection<Temperature>();
+            Collection<Temperature> temperaturesSecond = new Collection<Temperature>();
+            Collection<Temperature> temperaturesThird = new Collection<Temperature>();
+            Collection<Temperature> temperaturesFourth = new Collection<Temperature>();
+            openSheetAndProcess(xlWorkbook, ref temperaturesFirst, 1);
+            temperaturesFirst.Count.Output("Count");
+            /*
+            
+            openSheetAndProcess(xlWorkbook, ref temperatures, 2);
+            openSheetAndProcess(xlWorkbook, ref temperatures, 3);
+            openSheetAndProcess(xlWorkbook, ref temperatures, 4);
+
+            Console.WriteLine(temperatures.Count);
+            Console.WriteLine("Введите имя файла: ");
+            string file = Console.ReadLine();
+            dynamic[,] data = new dynamic[32, temperatures.Count + 2];
+            data[0, 0] = "Сравнение температур по датам четырех месяцев";
+            data[0, 1] = 1;
+            data[0, 2] = 2;
+            data[0, 3] = 3;
+            data[0, 4] = 4;
+
+            for (int i = 1; i <= 31; i++)
+            {
+                data[i, 0] = i;
+            }
+            ExcelExport(file + ".xlsx", data); */
+        }
+    }
+}
---
+++ UtilsUltra.cs
@@ -0,0 +1,375 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Threading;
+
+public static class Utils
+{
+    /*
+     *              _____                _____                    _____                    _____            _____          
+     *             /\    \              /\    \                  /\    \                  /\    \          /\    \         
+     *            /::\____\            /::\    \                /::\    \                /::\____\        /::\    \        
+     *           /:::/    /            \:::\    \               \:::\    \              /:::/    /       /::::\    \       
+     *          /:::/    /              \:::\    \               \:::\    \            /:::/    /       /::::::\    \      
+     *         /:::/    /                \:::\    \               \:::\    \          /:::/    /       /:::/\:::\    \     
+     *        /:::/    /                  \:::\    \               \:::\    \        /:::/    /       /:::/__\:::\    \    
+     *       /:::/    /                   /::::\    \              /::::\    \      /:::/    /        \:::\   \:::\    \   
+     *      /:::/    /      _____        /::::::\    \    ____    /::::::\    \    /:::/    /       ___\:::\   \:::\    \  
+     *     /:::/____/      /\    \      /:::/\:::\    \  /\   \  /:::/\:::\    \  /:::/    /       /\   \:::\   \:::\    \ 
+     *    |:::|    /      /::\____\    /:::/  \:::\____\/::\   \/:::/  \:::\____\/:::/____/       /::\   \:::\   \:::\____\
+     *    |:::|____\     /:::/    /   /:::/    \::/    /\:::\  /:::/    \::/    /\:::\    \       \:::\   \:::\   \::/    /
+     *     \:::\    \   /:::/    /   /:::/    / \/____/  \:::\/:::/    / \/____/  \:::\    \       \:::\   \:::\   \/____/ 
+     *      \:::\    \ /:::/    /   /:::/    /            \::::::/    /            \:::\    \       \:::\   \:::\    \     
+     *       \:::\    /:::/    /   /:::/    /              \::::/____/              \:::\    \       \:::\   \:::\____\    
+     *        \:::\__/:::/    /    \::/    /                \:::\    \               \:::\    \       \:::\  /:::/    /    
+     *         \::::::::/    /      \/____/                  \:::\    \               \:::\    \       \:::\/:::/    /     
+     *          \::::::/    /                                 \:::\    \               \:::\    \       \::::::/    /      
+     *           \::::/    /                                   \:::\____\               \:::\____\       \::::/    /       
+     *            \__/____/                                     \::/    /                \::/    /        \::/    /        
+     *                                                           \/____/                  \/____/          \/____/         
+     *                                                                                                                     
+     */
+    private static Stopwatch executionTime = new Stopwatch();
+    public static Random random = new Random();
+    public static ProgramInfo info;
+
+    /// <summary>
+    /// Вывести авторов + время выполнение программы
+    /// </summary>
+    public static void PrintAuthors()
+    {
+        Console.Title = info.name;
+        //Authors
+        Console.ForegroundColor = ConsoleColor.DarkGray;
+        Console.WriteLine("----------------------");
+        Console.WriteLine($"Задание #{info.name.Split('.')[0]}");
+        Console.WriteLine($"Выполнил {info.author}");
+        Console.WriteLine("ИСП211       ");
+        Console.WriteLine("----------------------");
+        Console.ResetColor();
+
+        //OTHER
+        executionTime.Start();
+        //on exit
+        AppDomain.CurrentDomain.ProcessExit += delegate (object sender, EventArgs e)
+        {
+            executionTime.Stop();
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("\n----------------------");
+            Console.WriteLine($"Время: {Math.Round(executionTime.Elapsed.TotalMilliseconds)}мс | {Math.Round(executionTime.Elapsed.TotalSeconds, 2)}с");
+        };
+
+        //HANDLE F1
+        WriteLineColored("\n<fg=gray>Нажмите <fg=darkyellow>F1 <fg=gray>для вывода информации о программе\n<fg=darkyellow>Любой символ<fg=gray> для продолжения");
+        Console.ForegroundColor = ConsoleColor.Black;
+        if (Console.ReadKey().Key == ConsoleKey.F1)
+        {
+            string accent = "<r><fg=green>";
+            string normal = "<r><fg=cyan>";
+            WriteLineColored($"\n{accent}Автор:{normal} " + info.author);
+            WriteLineColored($"{accent}Название:{normal} " + info.name);
+            WriteLineColored($"{accent}Описание:{normal} " + info.description);
+            WriteLineColored($"{accent}Инструкция:{normal} " + info.instruction + "\n");
+        }
+        else
+        {
+            Console.WriteLine();
+        }
+        Console.ResetColor();
+    }
+
+    /// <summary>
+    /// Быстрый ввод переменной + парсинг в переданный тип
+    /// </summary>
+    /// <typeparam name="T">Тип переменной</typeparam>
+    /// <param name="name">Имя переменной (опционально)</param>
+    /// <returns>Ввод с консоли</returns>
+    public static T Input<T>(string name = "?")
+    {
+        WriteColored($"Введите {name}<r>:\n");
+        return (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+    }
+    public static void Input<T>(this T variable, string name = "?")
+    {
+        WriteColored($"Введите {name}<r>:\n");
+        variable = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+    }
+
+    /// <summary>
+    /// Быстрый вывод переменной
+    /// </summary>
+    /// <typeparam name="T">Тип переменной (опционально, можно вызвать с переменной)</typeparam>
+    /// <param name="variable">Переменная</param>
+    /// <param name="name">Имя (опционально)</param>
+    public static void Output<T>(this T variable, string name = "")
+    {
+        WriteLineColored($"{name.Replace(':', '\0')}<r> = {variable}");
+    }
+    public static void Output<T>(this T[] variable, string name = "")
+    {
+        WriteLineColored($"{name.Replace(':', '\0')}<r> = {String.Join(", ", variable)}");
+    }
+    public static void OutputArray(this Array array, bool printIndices = false)
+    {
+        int maxLength = 0;
+        array.Map(element => { maxLength = Math.Max(maxLength, element.ToString().Length); });
+        if (array.Rank == 2)
+        {
+            for (int y = 0; y < array.GetLength(0); y++)
+            {
+                for (int x = 0; x < array.GetLength(1); x++)
+                {
+                    if (printIndices)
+                    {
+                        WriteColored($"<fg=DarkGray>[{y},{x}]:<r>{array.GetValue(y, x)}");
+                    }
+                    else
+                    {
+                        Console.Write(array.GetValue(y, x));
+                    }
+                    for (int i = 0; i <= maxLength - array.GetValue(y, x).ToString().Length + 1; i++) Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+        else
+        {
+            for (int x = 0; x < array.GetLength(0); x++)
+            {
+                if (printIndices)
+                {
+                    WriteColored($"<fg=DarkGray>[{x}]:<r>{array.GetValue(x)}");
+                }
+                else
+                {
+                    Console.Write(array.GetValue(x));
+                }
+                for (int i = 0; i <= maxLength - array.GetValue(x).ToString().Length + 1; i++) Console.Write(" ");
+            }
+        }
+        Console.WriteLine();
+    }
+
+    /// <summary>
+    /// Вывод красивой ошибки
+    /// </summary>
+    /// <param name="errorMessage">Содержание ошибки</param>
+    public static void PrintError(string errorMessage)
+    {
+        //Console.BackgroundColor = ConsoleColor.DarkRed;
+        //Console.WriteLine($"ошибка: {errorMessage}".ToUpper());
+        //Console.ResetColor();
+        WriteLineColored($"<bg=DarkRed>ошибка: {errorMessage}".ToUpper());
+    }
+
+
+    /// <summary>
+    /// Выполнение действие над массивом любой размерности
+    /// </summary>
+    /// <param name="array">Массив (можно вызвать из него)</param>
+    /// <param name="action">Само действие</param>
+    /// <param name="dimension"></param>
+    /// <param name="indices"></param>
+    public static void Map(this Array array, Func<object, object> action, int dimension = 0, int[] indices = null)
+    {
+        if (indices == null) indices = new int[array.Rank];
+
+        for (int i = 0; i < array.GetLength(dimension); i++)
+        {
+            indices[dimension] = i;
+
+            if (dimension == array.Rank - 1)
+            {
+                var newValue = action(array.GetValue(indices));
+                array.SetValue(newValue, indices);
+            }
+            else
+            {
+                Map(array, action, dimension + 1, indices);
+            }
+        }
+    }
+    public static void Map(this Array array, Action<object> action, int dimension = 0, int[] indices = null)
+    {
+        if (indices == null) indices = new int[array.Rank];
+
+        for (int i = 0; i < array.GetLength(dimension); i++)
+        {
+            indices[dimension] = i;
+
+            if (dimension == array.Rank - 1)
+            {
+                action(array.GetValue(indices));
+            }
+            else
+            {
+                Map(array, action, dimension + 1, indices);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Возвращает случайный элемент из массива или строки
+    /// </summary>
+    /// <typeparam name="T">Тип переменной (можно вызвать из массива)</typeparam>
+    /// <param name="array">Массив (можно вызвать из массива)</param>
+    /// <returns>Случайный элемент из массива</returns>
+    public static T PickRandom<T>(this Array array)
+    {
+        List<T> elements = new List<T>();
+        array.Map(item => elements.Add((T)item));
+        return elements[random.Next(elements.Count)];
+    }
+    public static string PickRandom(this string str)
+    {
+        return str[random.Next(str.Length)].ToString();
+    }
+
+    /// <summary>
+    /// Массив [][] => Массив [,]
+    /// </summary>
+    /// <typeparam name="T">Тип массива (можно вызвать из массива)</typeparam>
+    /// <param name="array">массив</param>
+    /// <returns>Массив [,]</returns>
+    public static T[,] ToArray<T>(this T[][] array)
+    {
+        int rows = array.Length;
+        int cols = array[0].Length;
+        T[,] rectArray = new T[rows, cols];
+
+        for (int y = 0; y < rows; y++)
+        {
+            for (int x = 0; x < cols; x++)
+            {
+                rectArray[y, x] = array[y][x];
+            }
+        }
+
+        return rectArray;
+    }
+
+    /// <summary>
+    /// Вывод цветного текста. <fg=color> - цвет текста, <bg=color> - цвет фона, <r> - сброс цвета
+    /// </summary>
+    /// <param name="text">Текст</param>
+    public static void WriteColored(string text, bool resetColor = true)
+    {
+        if (resetColor) text += "<r>";
+        string pattern = @"(<[^>]+>)|([^<]+)";
+        List<string> wordList = new List<string>();
+
+        foreach (Match match in Regex.Matches(text, pattern))
+        {
+            if (match.Groups[1].Success)
+            {
+                wordList.Add(match.Groups[1].Value);
+            }
+            else if (match.Groups[2].Success)
+            {
+                wordList.Add(match.Groups[2].Value);
+            }
+        }
+        Array words = wordList.ToArray();
+        words.Map(item =>
+        {
+            var itemString = item.ToString();
+            if (itemString[0] == '<' && itemString[itemString.Length - 1] == '>')
+            {
+                //tag
+                itemString = string.Concat(itemString.Where(c => !char.IsWhiteSpace(c))).ToLower();
+                if (itemString.Contains("="))
+                {
+                    var parts = itemString.Split('=');
+                    ConsoleColor color = new ConsoleColor();
+                    foreach (ConsoleColor c in (ConsoleColor[])Enum.GetValues(typeof(ConsoleColor)))
+                    {
+                        if (c.ToString().ToLower() + ">" == parts[1])
+                        {
+                            color = c;
+                        }
+                    }
+                    if (parts[0] == "<bg")
+                    {
+                        Console.BackgroundColor = color;
+                    }
+                    else if (parts[0] == "<fg")
+                    {
+                        Console.ForegroundColor = color;
+                    }
+                    else
+                    {
+                        Console.Write(item);
+                    }
+                }
+                else
+                {
+                    if (itemString == "<r>")
+                    {
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.Write(item);
+                    }
+                }
+            }
+            else
+            {
+                //word
+                Console.Write(item);
+            }
+        });
+    }
+    public static void WriteLineColored(string text)
+    {
+        WriteColored(text + "\n");
+    }
+
+    /// <summary>
+    /// struct с полезными константами
+    /// </summary>
+    public struct Constants
+    {
+        /// <summary>
+        /// abcdefghijklmnopqrstuvwxyz
+        /// </summary>
+        public static string lowerAlphabet => "abcdefghijklmnopqrstuvwxyz";
+        /// <summary>
+        /// ABCDEFGHIJKLMNOPQRSTUVWXYZ
+        /// </summary>
+        public static string upperAlphabet => lowerAlphabet.ToUpper();
+        /// <summary>
+        /// 0123456789
+        /// </summary>
+        public static string numbers => "0123456789";
+        /// <summary>
+        /// abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+        /// </summary>
+        public static string fullAlphabet => lowerAlphabet + upperAlphabet;
+        /// <summary>
+        /// 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+        /// </summary>
+        public static string alpabetNumbers => numbers + fullAlphabet;
+    }
+
+    /// <summary>
+    /// Информация о текущей программе
+    /// </summary>
+    public class ProgramInfo
+    {
+        public string author;
+        public string name;
+        public string description;
+        public string instruction;
+
+        public ProgramInfo(string author, string name, string description, string instruction)
+        {
+            this.author = author;
+            this.name = name;
+            this.description = description;
+            this.instruction = instruction;
+        }
+    }
+}
